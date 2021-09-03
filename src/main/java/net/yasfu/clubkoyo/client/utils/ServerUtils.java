059fcc3fa2b464bd8e3ae364ddcc12a8dc5794cb
Ethan Jones <ethan@yasfu.net>
2021-09-03 16:40:10 -0600

    connectedToMineClub

--- src/main/java/net/yasfu/clubkoyo/client/utils/ServerUtils.java
+++ src/main/java/net/yasfu/clubkoyo/client/utils/ServerUtils.java
@@ -1,12 +1,13 @@
 package net.yasfu.clubkoyo.client.utils;
 
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -14,11 +15,25 @@ import java.net.UnknownHostException;
 public class ServerUtils {
 
     public static final String address = "clubkoyo.mineclub.com";
+    public static final String address_trunc = ".mineclub.com";
 
     public static void connectMineClub()
             throws UnknownHostException {
 
         ClientConnection.connect(InetAddress.getByName(address), 25565, true);
+    }
+
+    public static boolean connectedToMineClub() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        return connectedToMineClub(client);
+    }
+
+    public static boolean connectedToMineClub(MinecraftClient client) {
+        ServerInfo serverInfo = client.getCurrentServerEntry();
+
+        if (serverInfo == null) return false;
+
+        return serverInfo.address.endsWith(address_trunc);
     }
 
     public static void openMineClubInventory(MinecraftClient client) {
